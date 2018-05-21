91780e328ec7a0656d56c7597b378f615bb9b06e
David Vielhuber <david@vielhuber.de>
2018-05-22 00:57:40 +0200

    integrated google analytics

--- _js/_page.js
+++ _js/_page.js
@@ -29,5 +29,43 @@ export default class Class1
         document.documentElement.classList.add(hlp.getDevice());
         document.documentElement.classList.add(hlp.getBrowser());    
     }
+
+    initGoogleAnalytics()
+    {
+        if(navigator.userAgent.indexOf('Speed Insights') === -1)
+        {
+            return;
+        }
+
+        let property = 'UA-12350231-1';
+
+        /* opt out */
+        let disableStr = 'ga-disable-'+property;
+        if( document.cookie.indexOf(disableStr + '=true') > -1 )
+        {
+            window[disableStr] = true;
+        }
+        document.addEventListener('click', (e) =>
+        {
+            if( e.target.tagName === 'A' && e.target.textContent === 'Google Analytics deaktivieren' )
+            {
+                alert('Google Analytics wurde deaktiviert');
+                document.cookie = disableStr + '=true; expires=Thu, 31 Dec 2099 23:59:59 UTC; path=/'; window[disableStr] = true;
+                e.preventDefault();
+            }
+        }, true);
+
+        /* tracking code */
+        var script = null;        
+        script = document.createElement('script');
+        script.src = 'https://www.googletagmanager.com/gtag/js?id='+property;
+        document.head.appendChild(script);
+        script = document.createElement('script');
+        script.innerHTML += 'window.dataLayer = window.dataLayer || [];';
+        script.innerHTML += 'var gtag = function() { dataLayer.push(arguments); };';
+        script.innerHTML += 'gtag(\'js\', new Date());';
+        script.innerHTML += 'gtag(\'config\', '+property+', { \'anonymize_ip\': true });';
+        document.head.appendChild(script);
+    }
     
 }
