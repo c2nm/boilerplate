239af817492c264f0999d210012df7598bc97e1a
David Vielhuber <david@vielhuber.de>
2019-01-30 00:28:08 +0100

    Remove postcss-url

--- postcss.config.js
+++ postcss.config.js
@@ -1,10 +1,6 @@
 module.exports = {
     map: false,
     plugins: {
-        'autoprefixer': {},
-        'postcss-url': {
-            url: 'inline',
-            basePath: ['../_assets/']
-        },
+        'autoprefixer': {}
     }
 }
