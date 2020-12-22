0faab3a9e53e8a9fdb73a0e8d603c1e0cc961133
David Vielhuber <david@vielhuber.de>
2020-12-22 01:26:15 +0100

    Improve inliner

--- postcss.config.js
+++ postcss.config.js
@@ -1,9 +1,12 @@
 module.exports = {
     map: false,
-    plugins: [
-        require('autoprefixer'),
-        require('tailwindcss'),
-        require('cssnano')({ preset: 'default' }),
-        require('postcss-url')({ url: 'inline', basePath: ['../_assets/', '../_fonts/'] })
-    ]
+    plugins:
+        process.env.NODE_ENV === 'production'
+            ? [
+                  require('autoprefixer'),
+                  require('tailwindcss'),
+                  require('cssnano')({ preset: 'default' }),
+                  require('postcss-url')({ url: 'inline', basePath: ['./', './_fonts/'] })
+              ]
+            : [require('tailwindcss')]
 };
