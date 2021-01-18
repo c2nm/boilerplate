6d9c0ed0cb22ccf08e79f0103c1abad68d37d34c
David Vielhuber <david@vielhuber.de>
2021-01-18 01:25:11 +0100

    Correctly embedded fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ module.exports = {
     purge: ['./_html/**/*.html', './**/*.php'],
     darkMode: false,
     theme: {
+        fontFamily: {
+            custom: ['"Open Sans"', 'sans-serif']
+        },
         extend: {}
     },
     variants: {},
