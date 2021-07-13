c5810905f1cc3a18d9ca855a7098d5d690f28d68
David Vielhuber <david@vielhuber.de>
2021-07-13 22:28:46 +0200

    Improve google page speed detection

--- _js/Page.js
+++ _js/Page.js
@@ -32,7 +32,7 @@ export default class Page {
         // try to detect page speed insights and delay loading of scripts
         if (
             navigator.userAgent.indexOf('Speed Insights') > -1 ||
-            (navigator.language === 'en-US' && navigator.platform.indexOf('Linux') > -1)
+            navigator.userAgent.indexOf('Chrome-Lighthouse') > -1
         ) {
             //return;
         }
