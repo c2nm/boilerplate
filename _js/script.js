d16dfac6eeb4237c590195c37a057343c91a89cb
David Vielhuber <david@vielhuber.de>
2022-11-18 01:30:57 +0100

    Refactoring js.

--- _js/script.js
+++ _js/script.js
@@ -6,77 +6,6 @@ import Page from './Page';
 import Module from './Module';
 import RouteX from './RouteX';
 
-/* polyfills */
-// we use "useBuiltIns": "usage" and have all es core features included
-// however, there are cases where we need to import manual polyfill (since they are intentionally not included)
-import 'whatwg-fetch'; // use fetch
-import 'element-closest'; // closest polyfill
-import 'mdn-polyfills/Node.prototype.remove'; // dom polyfill
-/* ... */
-// promise (150kb)
-// included in corejs via builtins: usage
-new Promise(resolve => {
-    console.log('PROMISE RESOLVED');
-    resolve();
-});
-// forEach (20kb)
-// included in corejs via builtins: usage
-document.addEventListener('DOMContentLoaded', () => {
-    document.querySelectorAll('div').forEach(() => {
-        console.log('1');
-    });
-});
-// fetch (50kb)
-// this is missing in corejs (see https://github.com/zloirock/core-js), so we need a manual polyfill for this
-fetch('https://httpbin.org/bytes/100').then(response => {
-    console.log(response);
-});
-// remove (20kb)
-// this is missing in corejs (see https://github.com/zloirock/core-js/issues/317#issuecomment-314691446), so we need a manual polyfill for this
-document.addEventListener('DOMContentLoaded', () => {
-    document.querySelector('br').remove();
-});
-/* end of polyfills */
-
-/* if you embed your scripts with async, it is not guaranteed, that window load or document ready tirggers */
-/* use this instead of the original functions */
-const ready = new Promise(resolve => {
-    if (document.readyState !== 'loading') {
-        return resolve();
-    } else {
-        document.addEventListener('DOMContentLoaded', () => {
-            return resolve();
-        });
-    }
-});
-ready.then(() => {
-    /* ... */
-});
-const load = new Promise(resolve => {
-    if (document.readyState === 'complete') {
-        return resolve();
-    } else {
-        window.addEventListener('load', () => {
-            return resolve();
-        });
-    }
-});
-load.then(() => {
-    /* ... */
-});
-
-/* standard way */
-const page = new Page();
-document.addEventListener('DOMContentLoaded', () => {
-    page.ready();
-    /* make publically available to run from outside for debugging purposes */
-    window.page = page;
-});
-
-window.addEventListener('load', e => {
-    page.load();
-});
-
 /* modular way */
 // singletons
 [Page].forEach(classes__value => {
@@ -153,12 +82,92 @@ window.addEventListener('load', e => {
     }
 });
 
+/* polyfills */
+/*
+// we use "useBuiltIns": "usage" and have all es core features included
+// however, there are cases where we need to import manual polyfill (since they are intentionally not included)
+import 'whatwg-fetch'; // use fetch
+import 'element-closest'; // closest polyfill
+import 'mdn-polyfills/Node.prototype.remove'; // dom polyfill
+// ...
+// promise (150kb)
+// included in corejs via builtins: usage
+new Promise(resolve => {
+    console.log('PROMISE RESOLVED');
+    resolve();
+});
+// forEach (20kb)
+// included in corejs via builtins: usage
+document.addEventListener('DOMContentLoaded', () => {
+    document.querySelectorAll('div').forEach(() => {
+        console.log('1');
+    });
+});
+// fetch (50kb)
+// this is missing in corejs (see https://github.com/zloirock/core-js), so we need a manual polyfill for this
+fetch('https://httpbin.org/bytes/100').then(response => {
+    console.log(response);
+});
+// remove (20kb)
+// this is missing in corejs (see https://github.com/zloirock/core-js/issues/317#issuecomment-314691446), so we need a manual polyfill for this
+document.addEventListener('DOMContentLoaded', () => {
+    document.querySelector('br').remove();
+});
+*/
+/* end of polyfills */
+
+/* modular way (without dependencies) */
+/* if you embed your scripts with async, it is not guaranteed, that window load or document ready tirggers */
+/* use this instead of the original functions */
+/*
+const ready = new Promise(resolve => {
+    if (document.readyState !== 'loading') {
+        return resolve();
+    } else {
+        document.addEventListener('DOMContentLoaded', () => {
+            return resolve();
+        });
+    }
+});
+ready.then(() => {
+    // ...
+});
+const load = new Promise(resolve => {
+    if (document.readyState === 'complete') {
+        return resolve();
+    } else {
+        window.addEventListener('load', () => {
+            return resolve();
+        });
+    }
+});
+load.then(() => {
+    // ...
+});
+*/
+
+/* most basic way */
+/*
+const page = new Page();
+document.addEventListener('DOMContentLoaded', () => {
+    page.ready();
+    // make publically available to run from outside for debugging purposes
+    window.page = page;
+});
+window.addEventListener('load', e => {
+    page.load();
+});
+*/
+
 /* when using the technique with loadCSS, use this instead */
+/*
 hlp.waitUntil('footer', 'position', 'relative').then(() => {
     console.log('DONE');
 });
+*/
 
 /* React */
+/*
 const MyComponent = () => {
     return (
         <div>
@@ -168,3 +177,4 @@ const MyComponent = () => {
     );
 };
 console.log(MyComponent);
+*/
