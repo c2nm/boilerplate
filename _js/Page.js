e75a4bf4b5e80e92359a44ca0dc3de4814839ee8
David Vielhuber <david@vielhuber.de>
2022-11-18 01:36:00 +0100

    Refactoring js.

--- _js/Page.js
+++ _js/Page.js
@@ -1,8 +1,12 @@
 export default class Page {
     constructor() {}
 
-    async ready() {}
-    async load() {}
+    async ready() {
+        console.log('ready');
+    }
+    async load() {
+        console.log('load');
+    }
 
     static async readyOnce() {}
     static async loadOnce() {}
