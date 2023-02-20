27330caaac3993bda744fd450c6c6dc7cf9c468b
David Vielhuber <david@vielhuber.de>
2023-02-20 08:57:33 +0100

    Add TailwindCSS CDN to dev environment.

--- _js/Page.js
+++ _js/Page.js
@@ -3,11 +3,24 @@ export default class Page {
 
     async ready() {
         console.log('ready');
+        this.addTailwindCDN();
     }
     async load() {
         console.log('load');
     }
 
-    static async readyOnce() {}
-    static async loadOnce() {}
+    static async readyOnce() {
+        console.log('readyOnce');
+    }
+    static async loadOnce() {
+        console.log('loadOnce');
+    }
+
+    addTailwindCDN() {
+        if (window.location.host.includes('local') || window.location.host.includes('close2dev')) {
+            let script = document.createElement('script');
+            script.src = 'https://cdn.tailwindcss.com';
+            document.head.appendChild(script);
+        }
+    }
 }
